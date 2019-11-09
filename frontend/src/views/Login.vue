9a0164b01b2368ddda923959671bc4eca833a2c0
KristLin <kristlin.unsw@gmail.com>
2019-11-09 14:24:39 +1100

    fix login error alert

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -87,7 +87,7 @@ export default {
         })
         .catch(err => {
           window.console.log(err.response);
-          this.$swal("Oops!", "Email or password is incorrcet!", "error");
+          this.$swal("Oops!", err.response.data, "error");
         });
     }
   }
